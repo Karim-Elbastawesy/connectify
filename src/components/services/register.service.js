6cbc32a002ecafd4e6f9111f373cd3f2b820063d
Karim-Elbastawesy <karimelbastawesy20@gmail.com>
2026-03-07 01:53:32 +0200

    fix baseURL remove v1

--- src/components/services/register.service.js
+++ src/components/services/register.service.js
@@ -1,6 +1,7 @@
 import api from "./api";
 
 export async function registerUser(dataForm) {
+  console.log("Sending to API:", dataForm);
   const { data } = await api.post("/users/signup", dataForm);
   return data;
 }
